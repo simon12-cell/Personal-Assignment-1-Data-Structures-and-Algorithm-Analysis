2517df090dbd7881534ad8f7504dff079da2803d
Simone Raphael Erus <Simoneraphaelerus@gmail.com>
2026-03-09 02:26:13 +0700

    initial commit

--- src/Tugas/Mahasiswa2.java
+++ src/Tugas/Mahasiswa2.java
@@ -5,44 +5,45 @@ public class Mahasiswa {
     private String jurusan;
     private double ipk;
 
-    // Constructor
-    public Mahasiswa(String nama, String nim, String jurusan, double ipk) {
+    // constructor
+    public Mahasiswa(String nama, String nim, String jurusan, double ipk){
         this.nama = nama;
         this.nim = nim;
         this.jurusan = jurusan;
         this.ipk = ipk;
     }
 
-    // Getter IPK
-    public double getIpk() {
-        return ipk;
-    }
-
-    // Setter IPK
-    public void setIpk(double ipk) {
-        this.ipk = ipk;
-    }
-
-    public String getNim() {
+    // getter nim
+    public String getNim(){
         return nim;
     }
 
-    // Method update IPK
-    public void updateIpk(double ipkBaru) {
+    // getter ipk
+    public double getIpk(){
+        return ipk;
+    }
+
+    // setter ipk
+    public void setIpk(double ipk){
+        this.ipk = ipk;
+    }
+
+    // method update ipk
+    public void updateIpk(double ipkBaru){
         this.ipk = ipkBaru;
     }
 
-    // Method cek kelulusan
-    public String cekKelulusan() {
-        if (ipk >= 3.00) {
+    // cek kelulusan
+    public String cekKelulusan(){
+        if(ipk >= 3.00){
             return "Lulus";
-        } else {
+        } else{
             return "Belum Lulus";
         }
     }
 
-    // Menampilkan data
-    public void tampilkanInfo() {
+    // tampilkan data
+    public void tampilkanInfo(){
         System.out.println("Nama: " + nama);
         System.out.println("NIM: " + nim);
         System.out.println("Jurusan: " + jurusan);
