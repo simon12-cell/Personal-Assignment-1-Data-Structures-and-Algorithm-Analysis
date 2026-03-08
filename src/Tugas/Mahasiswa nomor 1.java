d5b8e788b0e33a85b6a280563fcc9f146e267351
Simone Raphael Erus <Simoneraphaelerus@gmail.com>
2026-03-09 01:06:51 +0700

    initial commit

---
+++ src/Tugas/Mahasiswa nomor 1.java
@@ -0,0 +1,24 @@
+public class Mahasiswa {
+
+    String nama;
+    String nim;
+    String jurusan;
+    double ipk;
+
+    // Constructor untuk mengisi data mahasiswa
+    public Mahasiswa(String nama, String nim, String jurusan, double ipk) {
+        this.nama = nama;
+        this.nim = nim;
+        this.jurusan = jurusan;
+        this.ipk = ipk;
+    }
+
+    // Method untuk menampilkan informasi mahasiswa
+    public void tampilkanInfo() {
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM: " + nim);
+        System.out.println("Jurusan: " + jurusan);
+        System.out.println("IPK: " + ipk);
+        System.out.println();
+    }
+}
